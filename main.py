1855794f1dc2554bc100bd76dbe731a899c86748
JethroNatividad <natividad.jet@gmail.com>
2024-03-22 12:51:48 +0800

    add function get trending

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from pytrends.request import TrendReq
 
-# PH
-# Limit 
-pytrends = TrendReq(hl='en-PH', tz=480)
-trending = pytrends.trending_searches(pn='philippines')
+def get_trending():
+    pytrends = TrendReq(hl='en-PH', tz=480)
+    trending = pytrends.trending_searches(pn='philippines')
 
-# convert trending dataframe to list
-for trend in trending[0]:
-    print(trend)
+    trending_list = []
+    for trend in trending[0]:
+        trending_list.append(trend)
+    return trending_list
+
