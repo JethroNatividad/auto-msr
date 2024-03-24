c5f0ba2faffb0a34c06bd1e49d12a80bcce13224
JethroNatividad <natividad.jet@gmail.com>
2024-03-24 10:40:54 +0800

    Implement random scroll time

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from pytrends.request import TrendReq
 import pyautogui
+import random
 
 def get_trending():
     pytrends = TrendReq(hl='en-PH', tz=480)
-    trending = pytrends.trending_searches(pn='philippines')
+    # pn = 'philippines', 'united_states', 'worldwide'
+    # switch pn if you need more searches
+    trending = pytrends.trending_searches(pn='united_states') 
 
     trending_list = []
     for trend in trending[0]:
@@ -16,21 +19,22 @@ def main():
     for trend in trending_list:
         # x=316, y=108 for the search bar
         pyautogui.click(316, 108)
-        # ctrl+a to select all
         pyautogui.hotkey('ctrl', 'a')
-        # delete to clear the search bar
         pyautogui.press('delete')
-        # type the trending search
-        pyautogui.typewrite(trend, interval=0.3)
-        # enter to search
+        pyautogui.typewrite(trend, interval=0.1)
         pyautogui.press('enter')
-        # wait for the page to load
         pyautogui.sleep(5)
-        # scroll down a bit and scroll back again to the top
-        pyautogui.scroll(-200)
-        pyautogui.sleep(2)
-        pyautogui.scroll(200)
+        # scroll down a bit
+        total_scroll = 0
+        for _ in range(0, 3):
+            random_scroll_pos = random.randint(100, 500)
+            random_scroll_time = random.uniform(0.5, 1)
+            pyautogui.scroll(-random_scroll_pos)
+            pyautogui.sleep(random_scroll_time)
+            total_scroll += random_scroll_pos
+        # scroll back to the top
         pyautogui.sleep(1)
-
+        pyautogui.scroll(total_scroll)
+        pyautogui.sleep(1)
 main()
 
